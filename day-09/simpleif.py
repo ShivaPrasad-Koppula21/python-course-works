b89c14c4a4567d05fe961abcc7117943707798a5
Koppula Shivaprasad <shivaprasadkoppula21@gmail.com>
2026-09-06 21:07:19 +0530

    Delete day-09 directory

--- day-09/simpleif.py
+++
@@ -1,8 +0,0 @@
-'''sales=int(input())
-if sales>1000:
-    print("best seller")
-    '''
-eligi_acc= eval(input('eligible account'))
-ver_sub=eval(input('meta verified subscription')) 
-if eligi_acc and ver_sub:
-    print('verified badge granted')               
